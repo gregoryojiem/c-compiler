2fe6ee5f6f376bb1ecca22ffa96532c5316d7dbe
Gregory Ojiem <gro3228@rit.edu>
2024-07-14 15:15:14 -0400

    Updated with better error handling, and results are now less verbose

--- run-valid-tests.bat
+++ run-valid-tests.bat
@@ -6,36 +6,38 @@ set total_tests=0
 
 for /f "delims=" %%a in ('dir /b /s "CCompiler\examples\valid\*.*"') do (
   if "%%~xa"==".c" (
-    :: Use custom compiler, assemble and link with gcc, then run and capture the return value of main
-    echo Compiling: %%a
-    .\CCompiler\bin\Debug\net7.0\CCompiler.exe "%%a" 
-    echo Assembling: "%%~dpna.s"
-    gcc -c "%%~dpna.s" -o "%%~dpna.o" 
-    echo Linking: "%%~dpna.o"
-    gcc "%%~dpna.o" -o "%%~dpna" 
-    echo Running: "%%~dpna.exe"
-    call "%%~dpna.exe"
-    set ccompiler_return=!errorlevel!
-    echo Return value: !ccompiler_return!
-    
-    :: Test the result of custom compiler against the result of gcc compiler
-    gcc "%%~dpna.c" -o "%%~dpna_gcc.exe" 
-    call "%%~dpna_gcc.exe"
-    set gcc_return=!errorlevel!
-    if !errorlevel! == !gcc_return! (
-      echo Test result: PASS, return values match.
-      set /a tests_passed+=1
-    ) else (
-      echo Test result: FAIL, return values differ.
-    )
-    
-    :: Clean up the files produced
-    echo.
-    del "%%~dpna.s"
-    del "%%~dpna.o"
-    del "%%~dpna.exe"
-    del "%%~dpna_gcc.exe"
+    :: Test the custom compiler on each .c file in the given subdirectory
     set /a total_tests+=1
+    echo Compiling: %%a
+    .\CCompiler\bin\Debug\net7.0\CCompiler.exe "%%a"
+    if !errorlevel! lss 0 (
+        echo Test result: FAIL, compilation unsuccessful.
+    ) else (
+        :: Compilation was successful, so we assemble, link, and then run the .exe created
+        gcc -c "%%~dpna.s" -o "%%~dpna.o" 
+        gcc "%%~dpna.o" -o "%%~dpna" 
+        call "%%~dpna"
+        set ccompiler_return=!errorlevel!
+        echo Return value: !ccompiler_return!
+        
+        :: Test the result of custom compiler against the result of gcc compiler
+        gcc "%%~dpna.c" -o "%%~dpna_gcc.exe" 
+        call "%%~dpna_gcc.exe"
+        set gcc_return=!errorlevel!
+        if !errorlevel! == !gcc_return! (
+          echo Test result: PASS, return value matches gcc.
+          set /a tests_passed+=1
+        ) else (
+          echo Test result: FAIL, return value differs from gcc.
+        )
+        
+        :: Clean up the files produced
+        del "%%~dpna.s"
+        del "%%~dpna.o"
+        del "%%~dpna.exe"
+        del "%%~dpna_gcc.exe"
+    )
+    echo.
   )
 )
 
