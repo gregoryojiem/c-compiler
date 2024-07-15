92ea441f599b385bd668d42647b7191d981926e7
Gregory Ojiem <gro3228@rit.edu>
2024-07-15 19:46:03 -0400

    Lexer accounts for carriage return now

--- CCompiler/Lexer.cs
+++ CCompiler/Lexer.cs
@@ -74,6 +74,7 @@ public class Lexer
             {
                 case ' ':
                 case '\t':
+                case '\r':
                 case '\n':
                 case '/':
                     break;
