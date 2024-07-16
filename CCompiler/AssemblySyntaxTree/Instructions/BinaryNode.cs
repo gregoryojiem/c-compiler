da500964442fba7f25b483050a8c1974e2fb2ac4
Gregory Ojiem <gro3228@rit.edu>
2024-07-16 00:09:02 -0400

    Added asm instructions for binary operations

---
+++ CCompiler/AssemblySyntaxTree/Instructions/BinaryNode.cs
@@ -0,0 +1,65 @@
+﻿using CCompiler.AssemblySyntaxTree.Operands;
+
+namespace CCompiler.AssemblySyntaxTree.Instructions;
+
+public class BinaryNode : AsmInstructionNode, IAllocatableInstruction
+{
+    private TokenType _binaryOp;
+    private IOperand _src;
+    private IOperand _dst;
+
+    public BinaryNode(TokenType binaryOp, IOperand src, IOperand dst)
+    {
+        _binaryOp = binaryOp;
+        _src = src;
+        _dst = dst;
+    }
+
+    public void DoAllocationPass(Dictionary<string, int> variableMap, ref int stackPos)
+    {
+        if (_src is PseudoRegOp srcPseudo)
+        {
+            _src = IAllocatableInstruction.HandleAllocation(variableMap, ref stackPos, srcPseudo);
+        }
+
+        if (_dst is PseudoRegOp dstPseudo)
+        {
+            _dst = IAllocatableInstruction.HandleAllocation(variableMap, ref stackPos, dstPseudo);
+        }
+    }
+    
+    public void FixOperands(List<AsmInstructionNode> instructions)
+    {
+        if (_binaryOp is TokenType.Multiply && _dst is StackOp)
+        {
+            var originalDst = _dst;
+            var tempRegister = new RegOp(RegOp.Register.R11d);
+            _dst = tempRegister;
+            instructions.Add(new MovlNode(originalDst, tempRegister));
+            instructions.Add(this);
+            instructions.Add(new MovlNode(tempRegister, originalDst));
+            return;
+        }
+        if (_binaryOp is TokenType.Multiply)
+        {
+            instructions.Add(this);
+            return;
+        }
+        if (_src is not StackOp srcStackOp || _dst is not StackOp)
+        {
+            instructions.Add(this);
+            return;
+        }
+
+        var scratchRegister = new RegOp(RegOp.Register.R10d);
+        var tempVariable = new MovlNode(srcStackOp, scratchRegister);
+        _src = scratchRegister;
+        instructions.Add(tempVariable);
+        instructions.Add(this);
+    }
+    
+    public override string ConvertToAsm()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CCompiler/AssemblySyntaxTree/Instructions/DivlNode.cs
@@ -0,0 +1,37 @@
+﻿using CCompiler.AssemblySyntaxTree.Operands;
+
+namespace CCompiler.AssemblySyntaxTree.Instructions;
+
+public class DivlNode : AsmInstructionNode, IAllocatableInstruction
+{
+    private IOperand _divisor;
+
+    public DivlNode(IOperand divisor)
+    {
+        _divisor = divisor;
+    }
+
+    public void DoAllocationPass(Dictionary<string, int> variableMap, ref int stackPos)
+    {
+        if (_divisor is PseudoRegOp divisor)
+        {
+            _divisor = IAllocatableInstruction.HandleAllocation(variableMap, ref stackPos, divisor);
+        }
+    }
+    
+    public void FixConstantOps(List<AsmInstructionNode> fixedInstructions)
+    {
+        if (_divisor is ImmOp)
+        {
+            var scratchRegister = new RegOp(RegOp.Register.R10d);
+            fixedInstructions.Add(new MovlNode(_divisor, scratchRegister));
+            _divisor = scratchRegister;
+        }
+        fixedInstructions.Add(this);
+    }
+    
+    public override string ConvertToAsm()
+    {
+        throw new NotImplementedException();
+    }
+}
