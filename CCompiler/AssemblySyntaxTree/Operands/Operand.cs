83e693983a3d5315d0f1444fa7519752f660f0d5
Gregory Ojiem <gro3228@rit.edu>
2024-07-16 00:27:25 -0400

    Moved UnaryOps to Operand class, added BinaryOps

--- CCompiler/AssemblySyntaxTree/Operands/Operand.cs
+++ CCompiler/AssemblySyntaxTree/Operands/Operand.cs
@@ -14,4 +14,25 @@ public interface IOperand
             _ => throw new NotImplementedException()
         };
     }
+    
+    public static string TokenTypeToUnaryOp(TokenType tokenType)
+    {
+        return tokenType switch
+        {
+            TokenType.Negate => "negl",
+            TokenType.Complement => "notl",
+            _ => throw new NotImplementedException()
+        };
+    }
+    
+    public static string TokenTypeToBinaryOp(TokenType tokenType)
+    {
+        return tokenType switch
+        {
+            TokenType.Add => "addl",
+            TokenType.Negate => "subl",
+            TokenType.Multiply => "imull",
+            _ => throw new NotImplementedException()
+        };
+    }
 }
